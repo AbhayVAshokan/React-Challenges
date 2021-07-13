e136e925b1364cbcd5b929807ef97a6cc801bf6c
AbhayVAshokan <abhayvashokan@gmail.com>
2021-07-13 12:56:14 +0530

    Added longest common substring function

--- spotlight-search/src/App.tsx
+++ spotlight-search/src/App.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import InputField from "./components/InputField";
+import Suggestions from "./components/Suggestions";
+import words from "./resources/words";
+
 const App = () => {
-  return <div>Hello World</div>;
+  // Input value
+  const [value, setValue] = useState<string>("");
+
+  // Input placeholder
+  const [placeholder, setPlaceholder] = useState<string>("");
+
+  // Function to filter the suggestions according to the current input value
+  const getSuggestions = () => {};
+
+  return (
+    <>
+      <InputField />
+      <Suggestions />
+    </>
+  );
 };
 
 export default App;
